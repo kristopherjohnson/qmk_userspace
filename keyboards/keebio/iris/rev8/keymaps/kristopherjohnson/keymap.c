ac0e24d1eaeefd9a435bb05e13d20ec74efc93c4
Kris Johnson <kristopher.johnson@ditto.live>
2024-12-16 14:37:52 -0500

    Add tap-hold for LShift/Esc and RShift/Tab

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -10,38 +10,38 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default layer
     [0] = LAYOUT(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
-        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_LSFT, KC_RSFT, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                            KC_LALT, KC_LGUI, KC_ENT , KC_SPC , TL_LOWR, TL_UPPR
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                 KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                 KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
+        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                 KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , LSFT_T(KC_ESC), RSFT_T(KC_TAB), KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+                                            KC_LALT, KC_LGUI, KC_ENT        , KC_SPC        , TL_LOWR, TL_UPPR
     ),
 
     // lower
     [1] = LAYOUT(
-        KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
-        KC_GRV , KC_SLSH, KC_ASTR, KC_LCBR, KC_RCBR, KC_UNDS,                   _______, KC_P7  , KC_P8  , KC_P9  , KC_P0  , KC_PGDN,
-        _______, KC_MINS, KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL ,                   _______, KC_P4  , KC_P5  , KC_P6  , KC_PLUS, KC_PIPE,
-        _______, KC_CAPS, CW_TOGG, KC_LBRC, KC_RBRC, KC_MINS, _______, _______, _______, KC_P1  , KC_P2  , KC_P3  , KC_MINS, _______,
-                                            _______, _______, KC_BSPC, KC_DEL , _______, KC_P0
+        KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                 KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
+        KC_GRV , KC_SLSH, KC_ASTR, KC_LCBR, KC_RCBR, KC_UNDS,                                 _______, KC_P7  , KC_P8  , KC_P9  , KC_PLUS, KC_PGDN,
+        _______, KC_MINS, KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL ,                                 _______, KC_P4  , KC_P5  , KC_P6  , KC_P0  , KC_PIPE,
+        _______, KC_CAPS, CW_TOGG, KC_LBRC, KC_RBRC, KC_MINS, _______       , _______       , _______, KC_P1  , KC_P2  , KC_P3  , KC_MINS, _______,
+                                            _______, _______, KC_BSPC       , KC_DEL        , _______, KC_P0
     ),
 
     // raise
     [2] = LAYOUT(
-        KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5,                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-        _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-        _______, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                   KC_EQL , KC_HOME, KC_PGUP, KC_INS , KC_UP,   KC_DEL ,
-        _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, _______, _______, KC_PLUS, KC_END , KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,
-                                            _______, _______, _______, _______, _______, _______
+        KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5,                                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
+        _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                 KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        _______, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                                 KC_EQL , KC_HOME, KC_PGUP, KC_INS , KC_UP,   KC_DEL ,
+        _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, _______       , _______       , KC_PLUS, KC_END , KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,
+                                            _______, _______, _______       , _______       , _______, _______
     ),
 
     // raise+lowerk
     [3] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, EE_CLR , QK_RBT , _______,                   _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______,
-                                            _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, EE_CLR , QK_RBT , _______,                                 _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, QK_BOOT, _______       , _______       , _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______       , _______       , _______, _______
     )
 };
 // clang-format on
