baabe07deaf5a2e4849eb0e183165666ae34bdfd
Kris Johnson <kristopher.johnson@ditto.live>
2024-12-18 15:28:00 -0500

    Add tilde and backtick to inner positions on LOWER layer

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // lower
     [1] = LAYOUT(
         KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                 KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
-        KC_GRV , KC_SLSH, KC_ASTR, KC_LCBR, KC_RCBR, KC_UNDS,                                 _______, KC_P7  , KC_P8  , KC_P9  , KC_PLUS, KC_PGDN,
-        _______, KC_MINS, KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL ,                                 _______, KC_P4  , KC_P5  , KC_P6  , KC_P0  , KC_PIPE,
+        KC_GRV , KC_SLSH, KC_ASTR, KC_LCBR, KC_RCBR, KC_TILD,                                 KC_TILD, KC_P7  , KC_P8  , KC_P9  , KC_PLUS, KC_PGDN,
+        _______, KC_MINS, KC_PLUS, KC_LPRN, KC_RPRN, KC_GRV ,                                 KC_GRV , KC_P4  , KC_P5  , KC_P6  , KC_P0  , KC_PIPE,
         _______, KC_CAPS, CW_TOGG, KC_LBRC, KC_RBRC, KC_MINS, _______       , _______       , _______, KC_P1  , KC_P2  , KC_P3  , KC_MINS, _______,
                                             _______, _______, KC_BSPC       , KC_DEL        , _______, KC_P0
     ),
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______, _______       , _______       , _______, _______
     ),
 
-    // raise+lowerk
+    // raise+lower
     [3] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______,
         _______, _______, _______, EE_CLR , QK_RBT , _______,                                 _______, _______, _______, _______, _______, _______,
