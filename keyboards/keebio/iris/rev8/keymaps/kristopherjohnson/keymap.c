14b23f6860961bcaa74a4413d5e2cfd70ac5031d
Kris Johnson <kristopher.johnson@ditto.live>
2024-12-18 15:49:54 -0500

    Replace Esc in upper left with backtick/tilde

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -10,7 +10,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default layer
     [0] = LAYOUT(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                 KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
+        KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                 KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                 KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                 KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , LSFT_T(KC_ESC), RSFT_T(KC_TAB), KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
@@ -54,6 +54,7 @@ const uint16_t PROGMEM minus_combo[]     = {KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM equals_combo[]    = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM plus_combo[]      = {KC_F, KC_G, COMBO_END};
 const uint16_t PROGMEM bspc_combo[]      = {KC_ENT, KC_SPC, COMBO_END};
+const uint16_t PROGMEM esc_combo[]       = {KC_GRV, KC_1, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(left_combo, KC_LEFT),      //
@@ -64,6 +65,7 @@ combo_t key_combos[] = {
     COMBO(equals_combo, KC_EQUAL),   //
     COMBO(plus_combo, KC_PLUS),      //
     COMBO(bspc_combo, KC_BSPC),      //
+    COMBO(esc_combo, KC_ESC),        //
 };
 
 // Change LED colors when Caps Lock or Caps Word are active.
