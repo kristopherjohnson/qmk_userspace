9e56a4ee3d686ce6cd584e9dce16f744b5696b55
Kristopher Johnson <kris@kristopherjohnson.net>
2025-01-12 20:00:24 -0500

    Add tap-hold options for opt/-, cmd/=, and ctrl/enter

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                 KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                 KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , LSFT_T(KC_ESC), RSFT_T(KC_TAB), KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                            KC_LALT, KC_LGUI, KC_ENT        , KC_SPC        , TL_LOWR, TL_UPPR
+                           LALT_T(KC_MINS), LGUI_T(KC_EQUAL), LCTL_T(KC_ENT), KC_SPC        , TL_LOWR, TL_UPPR
     ),
 
     // lower
