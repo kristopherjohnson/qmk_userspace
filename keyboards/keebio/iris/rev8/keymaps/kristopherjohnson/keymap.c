02b92f16f9a69ed9c7cd06991628cb494b7fac41
Kris Johnson <kristopher.johnson@ditto.live>
2025-01-15 14:23:04 -0500

    Add home-row modifiers, HJKL arrow keys, and eliminate combos

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -10,63 +10,41 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default layer
     [0] = LAYOUT(
-        KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                 KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                 KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
-        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                 KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , LSFT_T(KC_ESC), RSFT_T(KC_TAB), KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                           LALT_T(KC_MINS), LGUI_T(KC_EQUAL), LCTL_T(KC_ENT), KC_SPC        , TL_LOWR, TL_UPPR
+        KC_GRV , KC_1        , KC_2        , KC_3        , KC_4        , KC_5       ,                                 KC_6   , KC_7        , KC_8        , KC_9        , KC_0           , KC_BSPC,
+        KC_TAB , KC_Q        , KC_W        , KC_E        , KC_R        , KC_T       ,                                 KC_Y   , KC_U        , KC_I        , KC_O        , KC_P           , KC_BSLS,
+        KC_LCTL, LT(3, KC_A) , LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G       ,                                 KC_H   , RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), LT(3, KC_SCLN) , KC_QUOT,
+        KC_LSFT, KC_Z        , KC_X        , KC_C        , KC_V        , KC_B       , LSFT_T(KC_ESC), RSFT_T(KC_TAB), KC_N   , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH        , KC_RSFT,
+                                                   LALT_T(KC_MINS), LGUI_T(KC_EQUAL), LCTL_T(KC_ENT), KC_SPC        , LT(1, KC_P0), LT(2, KC_PLUS)
     ),
 
     // lower
     [1] = LAYOUT(
-        KC_ESC , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                 KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL ,
-        _______, _______, _______, KC_LCBR, KC_RCBR, _______,                                 _______, KC_P7  , KC_P8  , KC_P9  , KC_PLUS, _______,
-        _______, _______, _______, KC_LPRN, KC_RPRN, _______,                                 _______, KC_P4  , KC_P5  , KC_P6  , KC_P0  , _______,
-        _______, KC_CAPS, CW_TOGG, KC_LBRC, KC_RBRC, _______, _______       , _______       , _______, KC_P1  , KC_P2  , KC_P3  , KC_MINS, _______,
-                                            _______, _______, KC_BSPC       , KC_DEL        , _______, _______
+        KC_ESC , KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_DEL ,
+        _______, _______     , _______     , KC_LCBR     , KC_RCBR     , _______    ,                                 _______, KC_P7       , KC_P8       , KC_P9       , KC_PLUS        , _______,
+        _______, _______     , _______     , KC_LPRN     , KC_RPRN     , _______    ,                                 _______, KC_P4       , KC_P5       , KC_P6       , KC_P0          , _______,
+        _______, KC_CAPS     , CW_TOGG     , KC_LBRC     , KC_RBRC     , _______    , _______       , _______       , _______, KC_P1       , KC_P2       , KC_P3       , KC_MINS        , _______,
+                                                                _______, _______    , KC_BSPC       , KC_DEL        , _______, KC_P0
     ),
 
     // raise
     [2] = LAYOUT(
-        KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5,                                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-        _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                 KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12,
-        _______, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                                 _______, KC_HOME, KC_PGUP, KC_INS , KC_UP,   KC_DEL ,
-        _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, _______       , _______       , _______, KC_END , KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,
-                                            _______, _______, _______       , _______       , _______, _______
+        KC_ESC , KC_F1       , KC_F2       , KC_F3       , KC_F4       , KC_F5      ,                                 KC_F6  , KC_F7       , KC_F8       , KC_F9       , KC_F10         , KC_F11 ,
+        _______, KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_F12 ,
+        _______, KC_MPRV     , KC_MNXT     , KC_VOLU     , KC_PGUP     , KC_UNDS    ,                                 _______, KC_HOME     , KC_PGUP     , KC_INS      , KC_UP          , KC_DEL ,
+        _______, KC_MSTP     , KC_MPLY     , KC_VOLD     , KC_PGDN     , KC_MINS    , _______       , _______       , _______, KC_END      , KC_PGDN     , KC_LEFT     , KC_DOWN        , KC_RGHT,
+                                                           _______     , _______    , _______       , _______       , _______, _______
     ),
 
-    // raise+lower
+    // raise+lower, or hold A, or hold semicolon
     [3] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, EE_CLR , QK_RBT , _______,                                 _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, QK_BOOT, _______       , _______       , _______, _______, _______, _______, _______, _______,
-                                            _______, _______, _______       , _______       , _______, _______
+        _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , _______     , _______        , KC_DEL ,
+        _______, _______     , _______     , EE_CLR      , QK_RBT      , _______    ,                                 _______, KC_PGUP     , KC_HOME     , KC_INS      , _______        , _______,
+        _______, _______     , _______     , _______     , _______     , _______    ,                                 KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT     , _______        , _______,
+        _______, _______     , _______     , _______     , _______     , QK_BOOT    , _______       , _______       , _______, KC_PGDN     , KC_END      , _______     , _______        , _______,
+                                                           _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     )
 };
 // clang-format on
-
-const uint16_t PROGMEM left_combo[]      = {KC_H, KC_J, COMBO_END};
-const uint16_t PROGMEM down_combo[]      = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM up_combo[]        = {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM right_combo[]     = {KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM minus_combo[]     = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM equals_combo[]    = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM plus_combo[]      = {KC_F, KC_G, COMBO_END};
-const uint16_t PROGMEM bspc_combo[]      = {KC_ENT, KC_SPC, COMBO_END};
-const uint16_t PROGMEM esc_combo[]       = {KC_GRV, KC_1, COMBO_END};
-
-combo_t key_combos[] = {
-    COMBO(left_combo, KC_LEFT),      //
-    COMBO(down_combo, KC_DOWN),      //
-    COMBO(up_combo, KC_UP),          //
-    COMBO(right_combo, KC_RIGHT),    //
-    COMBO(minus_combo, KC_MINS),     //
-    COMBO(equals_combo, KC_EQUAL),   //
-    COMBO(plus_combo, KC_PLUS),      //
-    COMBO(bspc_combo, KC_BSPC),      //
-    COMBO(esc_combo, KC_ESC),        //
-};
 
 // Change LED colors when Caps Lock or Caps Word are active.
 //
