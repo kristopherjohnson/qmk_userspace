5ac7e27b4e0e7f3227d17fef5d7ede99e3365f30
Kris Johnson <kristopher.johnson@ditto.live>
2025-01-29 17:01:48 -0500

    Tweaks to number layout, layer 3 home-row math keys

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -14,44 +14,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q        , KC_W        , KC_E        , KC_R        , KC_T       ,                                 KC_Y   , KC_U        , KC_I        , KC_O        , KC_P           , KC_BSLS,
         KC_LCTL, LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G       ,                                 KC_H   , RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT,
         KC_LSFT, KC_Z        , KC_X        , KC_C        , KC_V        , KC_B       , LSFT_T(KC_ESC), RSFT_T(KC_TAB), KC_N   , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH        , KC_RSFT,
-                                                   LALT_T(KC_MINS), LGUI_T(KC_EQUAL), LT(3, KC_ENT) , LT(4, KC_SPC) , LT(1, KC_P0), LT(2, KC_GRV)
+                                                   LALT_T(KC_MINS), LGUI_T(KC_EQUAL), LT(4, KC_ENT) , LT(3, KC_SPC) , LT(1, KC_P0), LT(2, KC_GRV)
     ),
 
     // lower
     [1] = LAYOUT(
         KC_ESC , KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_DEL ,
         _______, _______     , _______     , KC_LCBR     , KC_RCBR     , _______    ,                                 _______, KC_P7       , KC_P8       , KC_P9       , KC_MINS        , _______,
-        _______, _______     , _______     , KC_LPRN     , KC_RPRN     , _______    ,                                 _______, KC_P4       , KC_P5       , KC_P6       , KC_PLUS        , _______,
-        _______, KC_CAPS     , _______     , KC_LBRC     , KC_RBRC     , _______    , _______       , _______       , _______, KC_P1       , KC_P2       , KC_P3       , KC_MINS        , _______,
-                                                           _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
+        _______, _______     , _______     , KC_LPRN     , KC_RPRN     , _______    ,                                 KC_P0  , KC_P4       , KC_P5       , KC_P6       , KC_PLUS        , _______,
+        _______, KC_CAPS     , _______     , KC_LBRC     , KC_RBRC     , _______    , _______       , _______       , _______, KC_P1       , KC_P2       , KC_P3       , KC_ASTR        , _______,
+                                                           _______     , _______    , KC_BSPC       , KC_DEL        , KC_P0  , KC_GRV
     ),
 
     // raise
     [2] = LAYOUT(
         KC_ESC , KC_F1       , KC_F2       , KC_F3       , KC_F4       , KC_F5      ,                                 KC_F6  , KC_F7       , KC_F8       , KC_F9       , KC_F10         , KC_F11 ,
         _______, KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_F12 ,
-        _______, KC_LSFT     , KC_LCTL     , KC_LALT     , KC_LGUI     , _______    ,                                 _______, KC_HOME     , KC_PGUP     , KC_INS      , KC_UP          , KC_DEL ,
-        _______, _______     , _______     , _______     , KC_APP      , _______    , _______       , _______       , _______, KC_END      , KC_PGDN     , KC_LEFT     , KC_DOWN        , KC_RGHT,
+        _______, KC_LSFT     , KC_LCTL     , KC_LALT     , KC_LGUI     , _______    ,                                 _______, _______     , KC_PGUP     , KC_HOME     , KC_UP          , KC_END ,
+        _______, _______     , _______     , _______     , KC_APP      , _______    , _______       , _______       , _______, _______     , KC_PGDN     , KC_LEFT     , KC_DOWN        , KC_RGHT,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
 
-    // raise+lower, or hold Enter
+    // hold Space, or raise+lower
     [3] = LAYOUT(
+        _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , KC_LPRN     , KC_RPRN        , _______,
+        _______, KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LBRC     , KC_RBRC        , _______,
+        _______, KC_GRV      , KC_ASTR     , KC_MINS     , KC_EQUAL    , KC_PLUS    ,                                 _______, KC_RGUI     , KC_RALT     , KC_RCTL     , KC_RSFT        , _______,
+        _______, _______     , _______     , _______     , _______     , _______    , _______       , _______       , _______, _______     , _______     , _______     , _______        , _______,
+                                                           _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
+    ),
+
+    // hold Enter
+    [4] = LAYOUT(
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , _______     , _______        , KC_DEL ,
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , KC_LBRC     , KC_RBRC        , _______,
         _______, _______     , _______     , _______     , _______     , _______    ,                                 KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT     , KC_RGHT        , _______,
         _______, _______     , _______     , _______     , _______     , _______    , _______       , _______       , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END      , _______        , _______,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
-
-    // hold Space
-    [4] = LAYOUT(
-        _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , KC_LPRN     , KC_RPRN        , _______,
-        _______, KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LBRC     , KC_RBRC        , _______,
-        _______, _______     , _______     , KC_MINS     , KC_PLUS     , _______    ,                                 _______, KC_RGUI     , KC_RALT     , KC_RCTL     , KC_RSFT        , _______,
-        _______, _______     , _______     , _______     , _______     , _______    , _______       , _______       , _______, _______     , _______     , _______     , _______        , _______,
-                                                           _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
-    )
 };
 // clang-format on
 
