68f10e860cd8a15b6a65c3e16222203e762ad4dc
Kristopher Johnson <kris@kristopherjohnson.net>
2025-02-01 21:23:36 -0500

    Add Enter to lower right of layer-2 keypad

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC , KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_DEL ,
         _______, _______     , _______     , KC_LCBR     , KC_RCBR     , _______    ,                                 _______, KC_P7       , KC_P8       , KC_P9       , KC_MINS        , _______,
         _______, _______     , _______     , KC_LPRN     , KC_RPRN     , _______    ,                                 KC_P0  , KC_P4       , KC_P5       , KC_P6       , KC_PLUS        , _______,
-        _______, KC_CAPS     , _______     , KC_LBRC     , KC_RBRC     , _______    , _______       , _______       , _______, KC_P1       , KC_P2       , KC_P3       , KC_ASTR        , _______,
+        _______, KC_CAPS     , _______     , KC_LBRC     , KC_RBRC     , _______    , _______       , _______       , _______, KC_P1       , KC_P2       , KC_P3       , KC_ENTER       , _______,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , KC_P0  , KC_GRV
     ),
 
