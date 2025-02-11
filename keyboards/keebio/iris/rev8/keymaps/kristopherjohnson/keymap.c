c6a9086859f899587861443f7514e6f0218860e8
Kris Johnson <kristopher.johnson@ditto.live>
2025-02-11 16:19:05 -0500

    Add left/right arrow keys on upper thumb buttons

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -13,43 +13,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV , KC_1        , KC_2        , KC_3        , KC_4        , KC_5       ,                                 KC_6   , KC_7        , KC_8        , KC_9        , KC_0           , KC_BSPC,
         KC_TAB , KC_Q        , KC_W        , KC_E        , KC_R        , KC_T       ,                                 KC_Y   , KC_U        , KC_I        , KC_O        , KC_P           , KC_BSLS,
         KC_LCTL, LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G       ,                                 KC_H   , RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT,
-        KC_LSFT, KC_Z        , KC_X        , KC_C        , KC_V        , KC_B       , LSFT_T(KC_ESC), RSFT_T(KC_TAB), KC_N   , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH        , KC_RSFT,
+        KC_LSFT, KC_Z        , KC_X        , KC_C        , KC_V        , KC_B       , LT(4, KC_ESC) , LT(3, KC_TAB) , KC_N   , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH        , KC_RSFT,
                                                    LALT_T(KC_MINS), LGUI_T(KC_EQUAL), LT(4, KC_ENT) , LT(3, KC_SPC) , LT(1, KC_P0), LT(2, KC_GRV)
     ),
 
-    // lower
+    // Lower
+    // Numeric keypad on right side, with symbols on the left
     [1] = LAYOUT(
         KC_ESC , KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_DEL ,
         _______, _______     , _______     , KC_LCBR     , KC_RCBR     , _______    ,                                 _______, KC_P7       , KC_P8       , KC_P9       , KC_MINS        , _______,
-        _______, _______     , _______     , KC_LPRN     , KC_RPRN     , _______    ,                                 KC_P0  , KC_P4       , KC_P5       , KC_P6       , KC_PLUS        , _______,
-        _______, KC_CAPS     , _______     , KC_LBRC     , KC_RBRC     , _______    , _______       , _______       , _______, KC_P1       , KC_P2       , KC_P3       , KC_ENTER       , _______,
+        _______, KC_GRV      , KC_ASTR     , KC_LPRN     , KC_RPRN     , KC_PLUS    ,                                 KC_P0  , KC_P4       , KC_P5       , KC_P6       , KC_PLUS        , _______,
+        _______, KC_CAPS     , _______     , KC_LBRC     , KC_RBRC     , _______    , KC_LEFT       , KC_RGHT       , KC_P0  , KC_P1       , KC_P2       , KC_P3       , KC_ENTER       , _______,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , KC_P0  , KC_GRV
     ),
 
-    // raise
+    // Raise
+    // Arrow keys on right side
     [2] = LAYOUT(
         KC_ESC , KC_F1       , KC_F2       , KC_F3       , KC_F4       , KC_F5      ,                                 KC_F6  , KC_F7       , KC_F8       , KC_F9       , KC_F10         , KC_F11 ,
         _______, KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_F12 ,
         _______, KC_LSFT     , KC_LCTL     , KC_LALT     , KC_LGUI     , _______    ,                                 _______, _______     , KC_PGUP     , KC_HOME     , KC_UP          , KC_END ,
-        _______, _______     , _______     , _______     , KC_APP      , _______    , _______       , _______       , _______, _______     , KC_PGDN     , KC_LEFT     , KC_DOWN        , KC_RGHT,
+        _______, _______     , _______     , _______     , KC_APP      , _______    , KC_LEFT       , KC_RGHT       , _______, _______     , KC_PGDN     , KC_LEFT     , KC_DOWN        , KC_RGHT,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
 
-    // hold Space, or raise+lower
+    // Hold Space, or raise+lower
+    // "Symbols layer" with symbols on the left and right
     [3] = LAYOUT(
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , KC_LPRN     , KC_RPRN        , _______,
-        _______, KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LBRC     , KC_RBRC        , _______,
+        _______, KC_EXLM     , KC_AT       , KC_LCBR     , KC_RCBR     , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LBRC     , KC_RBRC        , _______,
         _______, KC_GRV      , KC_ASTR     , KC_MINS     , KC_EQUAL    , KC_PLUS    ,                                 _______, KC_RGUI     , KC_RALT     , KC_RCTL     , KC_RSFT        , _______,
-        _______, _______     , _______     , _______     , _______     , _______    , _______       , _______       , _______, _______     , _______     , _______     , _______        , _______,
+        _______, _______     , _______     , KC_LBRC     , KC_RBRC     , _______    , KC_LEFT       , KC_RGHT       , _______, _______     , _______     , _______     , _______        , _______,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
 
-    // hold Enter
+    // Hold Enter
+    // "Navigation layer" with arrow keys and home/end/pgup/pgdn on right
     [4] = LAYOUT(
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , _______     , _______        , KC_DEL ,
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , KC_LBRC     , KC_RBRC        , _______,
-        _______, _______     , _______     , _______     , _______     , _______    ,                                 KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT     , KC_RGHT        , _______,
-        _______, _______     , _______     , _______     , _______     , _______    , _______       , _______       , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END      , _______        , _______,
+        _______, KC_LSFT     , KC_LCTL     , KC_LALT     , KC_LGUI     , _______    ,                                 KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT     , KC_RGHT        , _______,
+        _______, _______     , _______     , _______     , _______     , _______    , KC_LEFT       , KC_RGHT       , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END      , _______        , _______,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
 };
