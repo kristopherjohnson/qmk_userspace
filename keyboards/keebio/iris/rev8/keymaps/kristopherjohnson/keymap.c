a09c9c5b7f8c3c266e86484c79fd330f422b43e2
Kristopher Johnson <kris@kristopherjohnson.net>
2025-02-23 18:41:50 -0500

    Make Z key CAPS LOCK for all layers except 0

--- keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
+++ keyboards/keebio/iris/rev8/keymaps/kristopherjohnson/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC , KC_F1       , KC_F2       , KC_F3       , KC_F4       , KC_F5      ,                                 KC_F6  , KC_F7       , KC_F8       , KC_F9       , KC_F10         , KC_F11 ,
         _______, KC_EXLM     , KC_AT       , KC_HASH     , KC_DLR      , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LPRN     , KC_RPRN        , KC_F12 ,
         _______, KC_LSFT     , KC_LCTL     , KC_LALT     , KC_LGUI     , _______    ,                                 _______, _______     , KC_PGUP     , KC_HOME     , KC_UP          , KC_END ,
-        _______, _______     , _______     , _______     , KC_APP      , _______    , KC_LEFT       , KC_RGHT       , _______, _______     , KC_PGDN     , KC_LEFT     , KC_DOWN        , KC_RGHT,
+        _______, KC_CAPS     , _______     , _______     , KC_APP      , _______    , KC_LEFT       , KC_RGHT       , _______, _______     , KC_PGDN     , KC_LEFT     , KC_DOWN        , KC_RGHT,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
 
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , KC_LPRN     , KC_RPRN        , _______,
         _______, KC_EXLM     , KC_AT       , KC_LCBR     , KC_RCBR     , KC_PERC    ,                                 KC_CIRC, KC_AMPR     , KC_ASTR     , KC_LBRC     , KC_RBRC        , _______,
         _______, KC_GRV      , KC_ASTR     , KC_MINS     , KC_EQUAL    , KC_PLUS    ,                                 _______, KC_RGUI     , KC_RALT     , KC_RCTL     , KC_RSFT        , _______,
-        _______, _______     , _______     , KC_LBRC     , KC_RBRC     , _______    , KC_LEFT       , KC_RGHT       , _______, _______     , _______     , _______     , _______        , _______,
+        _______, KC_CAPS     , _______     , KC_LBRC     , KC_RBRC     , _______    , KC_LEFT       , KC_RGHT       , _______, _______     , _______     , _______     , _______        , _______,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
 
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , _______     , _______        , KC_DEL ,
         _______, _______     , _______     , _______     , _______     , _______    ,                                 _______, _______     , _______     , KC_LBRC     , KC_RBRC        , _______,
         _______, KC_LSFT     , KC_LCTL     , KC_LALT     , KC_LGUI     , _______    ,                                 KC_LEFT, KC_DOWN     , KC_UP       , KC_RGHT     , KC_RGHT        , _______,
-        _______, _______     , _______     , _______     , _______     , _______    , KC_LEFT       , KC_RGHT       , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END      , _______        , _______,
+        _______, KC_CAPS     , _______     , _______     , _______     , _______    , KC_LEFT       , KC_RGHT       , KC_HOME, KC_PGDN     , KC_PGUP     , KC_END      , _______        , _______,
                                                            _______     , _______    , KC_BSPC       , KC_DEL        , _______, _______
     ),
 };
